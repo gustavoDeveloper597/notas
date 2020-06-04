91ee3052945e5b70201357da6453578ec43af3b8
Gustavo <ab1d@hotmail.es>
2020-06-04 14:15:45 -0500

    starting

---
+++ app/db.js
@@ -0,0 +1,69 @@
+console.log('db')
+window.notas=[];
+$(document).ready(function () {
+    let db = new PouchDB('notas');
+    db.changes({
+        since: 'now',
+        live: true
+    }).on('change', cargaNotas);
+
+    $('#nuevaNota').click(function () {
+        $('#nota').val('');
+        $('#seccion-nota').removeClass('oculta');
+        $('#seccion-notas').addClass('oculta');
+
+    });
+    $('#guardaNota').click(function () {
+
+        let nota = {
+            _id: new Date().toISOString(),
+            nota: $('#nota').val()
+        }
+        if (nota.nota.length > 0) {
+            db.put(nota).then(res => {
+                $('#seccion-nota').addClass('oculta');
+                $('#seccion-notas').removeClass('oculta');
+                alert('save');
+            });
+        } else {
+            alert('Escribe algo en la nota')
+        }
+
+    });
+
+    $(document).on('click','.delete',function () {
+        let id=$(this).attr('data-id');
+        let nota=window.notas.find(n=>{
+
+            return n._id==id;
+        })
+        console.log(nota)
+        db.remove(nota);
+    })
+
+    function cargaNotas(){
+        db.allDocs({include_docs: true, descending: true}).then(doc => {
+            window.notas=[];
+            let li='';
+            doc.rows.forEach(row=>{
+                window.notas.push(row.doc);
+                li+=drawNota(row.doc);
+            });
+            $('#notas').html('');
+            $('#notas').append(li);
+
+        })
+    }
+
+    function drawNota(nota){
+        console.log(nota)
+        return ` <li class="list-group-item" data-id="${nota._id}">
+                    ${nota.nota} <button class="btn btn-sm btn-danger pull-right delete" data-id="${nota._id}">Delete</button>
+                  
+                  </li>`;
+    }
+
+    cargaNotas()
+
+});
+
---
+++ sw.js
@@ -0,0 +1,117 @@
+console.log('service sw')
+var offline = new Response(
+    `<p>No tienes internet</p>`,
+    {
+        headers: {
+            'Content-Type': 'text/html'
+        }
+    }
+);
+
+
+
+// implementando estrategias de cache
+var STATIC='cache-estatico-v1';
+var DINAMIC='cache-dinamico-v1';
+
+let app_shell_cacheStatic=[
+    '/',
+    'https://code.jquery.com/jquery-3.4.1.slim.min.js',
+    'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js',
+    '//cdn.jsdelivr.net/npm/pouchdb@7.1.1/dist/pouchdb.min.js',
+    'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css',
+    'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js',
+    'styles.css',
+    'app/db.js',
+    'app/app.js',
+    'index.html',
+    '404.html',
+
+];
+
+
+self.addEventListener('install',e=>{
+
+    var cacheStatic=caches.open(STATIC).then(cache=>{
+        return cache.addAll(app_shell_cacheStatic);
+    });
+
+    e.waitUntil(Promise.all([cacheStatic]));
+
+});
+
+self.addEventListener('activate', e => {
+    console.log('axtivated..')
+    const respuesta = caches.keys().then(keys => {
+        keys.forEach(key => {
+            if (key != STATIC && key.includes('static')) {
+                return caches.delete(key);
+            }
+            if (key != DINAMIC && key.includes('dinamic')) {
+                return caches.delete(key);
+            }
+
+        });
+    });
+    e.waitUntil(respuesta);
+});
+
+
+
+self.addEventListener('fetch', e => {
+    let respuesta;
+    // let respuesta = fetch(e.request).then(resp => {
+    //     return resp.clone();
+    // });
+
+
+    // 1- guardo en cache los request
+    // let respuesta=caches.open(DINAMIC).then(cache=>{
+    //     console.log('open cache')
+    //     return fetch(e.request).then(red=>{
+    //         console.log('guardando en el cache dinamico');
+    //         cache.put(e.request,red.clone());
+    //         return  red.clone();// cache.match(e.request);
+    //     });
+    // });
+
+    // 2- red y cache
+    respuesta=fetch(e.request).then(red=>{
+        if(!red.ok){
+            return caches.match(e.request);
+        }else{
+            caches.open(DINAMIC).then(cache=>{
+               cache.put(e.request,red.clone());
+            });
+            return red.clone();
+        }
+    })
+
+    // 3-cache y red casi seria para aplicaciones solo en cache
+    // respuesta=caches.match(e.request).then(local=>{
+    //     if(local){
+    //         return local;
+    //     }else{
+    //         return fetch(e.request).then(red=>{
+    //            caches.open(DINAMIC).then(cache=>{
+    //               cache.put(e.request,red.clone());
+    //            });
+    //            return red.clone();
+    //         });
+    //     }
+    // });
+
+
+    // 4-solo cache
+     //respuesta=caches.match(e.request);
+
+
+    //
+    // 5-mi variacion busco en cache todo lo estatico si lo tengo lo doy si es dinamico busco en red y actualizo cache
+    // si falla en red busco en cache
+
+
+   // respuesta=fetch(e.request);
+
+    e.respondWith(respuesta);
+})
